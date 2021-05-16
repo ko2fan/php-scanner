d38ec8a94021cfd808f3d26f38ff763ab4dad75b
David Athay <ko2fan@gmail.com>
2021-05-16 22:10:24 +0100

    Moved to multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,95 @@
-use std::{collections::HashMap, error::Error, ffi::OsString, time::SystemTime, thread};
+use std::{collections::HashMap, error::Error, ffi::OsString, sync::Arc, thread, time::SystemTime};
 use yara::*;
 use glob::glob;
 
-use slog::{info, error, debug};
+use slog::{Logger, debug, error, info};
 use sloggers::Build;
 use sloggers::file::FileLoggerBuilder;
 
-struct App<'r> {
-    progress: u16,
-    files_scanned: usize,
-    file_list: Vec<OsString>,
-    results: HashMap<OsString, Vec<Rule<'r>>>,
-    rules: Rules,
-    logger: slog::Logger,
+fn log_matches(logger: &Logger, identifier: &str, file: &OsString) {
+    info!(logger, "Found {} in {}", identifier, file.to_str().unwrap());
 }
 
-impl<'r> App<'r> {
-    fn new(rules: Rules, builder: FileLoggerBuilder) -> App<'r> {
-        App {
-            progress: 0,
-            files_scanned: 0,
-            file_list: Vec::new(),
-            results: HashMap::new(),
-            rules: rules,
-            logger: builder.build().unwrap(),
+fn build_file_list(directory : &str) -> Vec<OsString> {
+    let mut result = Vec::new();
+    let pattern = String::from(directory) + "/**/*.php";
+    for entry in glob(pattern.as_str()).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => result.push(path.into_os_string()),
+            Err(e) => println!("{:?}", e),
         }
     }
 
-    fn update(&mut self) -> Result<(), Box<dyn Error>> {
-        self.progress = (100.0 - ((self.file_list.len() - self.files_scanned) as f32 / self.file_list.len() as f32 * 100.0)) as u16;
-
-        if self.files_scanned >= self.file_list.len() {
-            self.progress = 100;
-            println!("Scanning finished");
-        }
-
-        Ok(())
-    }
-
-    fn run_scan(&mut self) {
-        let file = self.file_list.get(self.files_scanned).expect("Scanned more files than existed");
-        let printable_file = file.clone().into_string().expect("Invalid path");
-        debug!(self.logger, "Scanning: {}", printable_file);
-        println!("Scanning {}",printable_file);
-        let scan_result = self.rules.scan_file(file, 5).unwrap();
-        self.results.insert(file.clone(), scan_result);
-        self.files_scanned += 1;
-    }
-
-    fn build_file_list(&mut self, directory : &str) {
-        info!(self.logger, "Scanning: {}", directory);
-        let pattern = String::from(directory) + "/**/*.php";
-        for entry in glob(pattern.as_str()).expect("Failed to read glob pattern") {
-            match entry {
-                Ok(path) => self.file_list.push(path.into_os_string()),
-                Err(e) => println!("{:?}", e),
-            }
-        }
-    }
-
-    fn log_matches(&self, identifier: &str, file: &OsString) {
-        info!(self.logger, "Found {} in {}", identifier, file.to_str().unwrap());
-    }
+    result
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = clap::App::new("php-scanner")
-        .version("0.1")
+        .version("0.2")
         .author("David Athay <ko2fan@gmail.com>")
         .about("Scans files for php malware")
         .args_from_usage("<directory> 'Sets the directory to scan'")
         .get_matches();
 
     let builder = FileLoggerBuilder::new("scan.log");
+    let logger = builder.build().unwrap();
 
     let mut compiler = Compiler::new()?;
     compiler.add_rules_file("php.yar")?;
-    let rules = compiler.compile_rules()?;
+    let rules = Arc::new(compiler.compile_rules()?);
 
-    let mut app = App::new(rules, builder);
+    let mut files_list = Vec::new();
+    let mut files_scanned = 0;
+
+    let mut results = HashMap::new();
 
     if let Some(dir) = matches.value_of("directory") {
         println!("Scanning: {}", dir);
-        app.build_file_list(dir);
+        info!(logger, "Scanning: {}", dir);
+        files_list = build_file_list(dir);
     }
 
     // start timing
     let timer = SystemTime::now();
 
+    let mut progress = 0;
+
     loop {
 
-        app.run_scan();
+        // app.run_scan();
+        let num_threads;
+        let files_left = files_list.len() - files_scanned;
+        if files_left > 5 {
+            num_threads = 5;
+        } else if files_left > 0 {
+            num_threads = files_left;
+        } else {
+            break;
+        }
 
-        app.update()?;
+        let mut children = Vec::new();
+        let mut files_to_scan: Vec<OsString> = Vec::new();
 
-        if app.progress == 100 {
+        files_to_scan.extend_from_slice(&files_list[files_scanned..files_scanned+num_threads]);
+
+        for t in 0..num_threads {
+            let file = files_to_scan[t].clone();
+            let rules = rules.clone();
+            children.push(thread::spawn(move || -> (OsString, Vec<Rule>) {
+                (file.clone(), rules.scan_file(&file, 5).unwrap())
+            }));
+        }
+
+        for c in children {
+            let result = c.join().unwrap();
+            results.insert(result.0, result.1);
+            files_scanned += 1;
+        }
+
+        // app.update()?;
+        progress = (100.0 - ((files_list.len() - files_scanned) as f32 / files_list.len() as f32 * 100.0)) as u16;
+
+        if progress == 100 {
             break;
         }
     }
@@ -103,14 +98,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let elapsed = format!("{}.{}", time_elapsed.as_secs().to_string(), time_elapsed.as_millis().to_string());
 
     let mut num_matches = 0;
-    for (file, entry) in &app.results {
+    for (file, entry) in &results {
         num_matches += entry.len();
         for rule in entry {
-            app.log_matches(rule.identifier, file);
+            log_matches(&logger, rule.identifier, file);
         }
     }
     
-    println!("Scanned {} files and found {} matches in {} seconds", app.files_scanned, num_matches, elapsed);
+    println!("Scanned {} files and found {} matches in {} seconds", files_scanned, num_matches, elapsed);
 
     Ok(())
 }
